f4c9766c886594fd1f217980ca37453161bf1a50
anantgarg <anantgarg@Anants-MacBook-Pro.local>
2020-08-18 19:56:11 -0600

    Update

---
+++ ios/obj/Release/LinkWithAttributes.cs
@@ -0,0 +1,3 @@
+[assembly: ObjCRuntime.LinkWith ("CometChatPro.framework", SmartLink = true, Frameworks = "UIKit Foundation")]
+[assembly: ObjCRuntime.LinkWith ("JitsiMeet.framework", SmartLink = true, Frameworks = "UIKit Foundation")]
+[assembly: ObjCRuntime.LinkWith ("WebRTC.framework", SmartLink = true, Frameworks = "Foundation UIKit")]
